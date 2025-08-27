9c2ec2de27c2b89bdf1391f835b5da8a4cc731bd
Pol Hernandez <polhv@proton.me>
2025-08-27 18:33:50 +0200

    initial wip

---
+++ cpp09/ex02/src/main.cpp
@@ -0,0 +1,43 @@
+//#include "../include/RPN.hpp"
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+typedef vector<vector<int >> VecOfIntVec;
+
+void printVecOfVec(VecOfIntVec vecOfVec) {
+    std::cout << "[";
+    for (VecOfIntVec::iterator vIt = vecOfVec.begin(); vIt != vecOfVec.end(); ++vIt) {
+        std::cout << "[" ;
+        for (vector<int>::iterator it = vIt->begin(); it != vIt->end(); ++it) {
+            std::cout << *it;
+            if (std::next(it) != vIt->end())
+                std::cout << ", ";
+        }
+        std::cout << "]";
+        if (std::next(vIt) != vecOfVec.end())
+            std::cout << ", ";
+    }
+    std::cout << "]" << std::endl;
+}
+
+int main(int argc, char **argv) {
+    if (argc<2) {
+        return 1;
+    }
+    VecOfIntVec vecOfVec;
+    for (int i=1; i < argc; i++) {
+        vector<int> vec;
+        int n;
+        std::stringstream ss(argv[i]);
+        if (!(ss >> n))
+            return 1;
+        vec.push_back(n);
+        vecOfVec.push_back(vec);
+    }
+    printVecOfVec(vecOfVec);
+    return 0;
+}
