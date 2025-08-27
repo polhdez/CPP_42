b43d56b560d68f7df4096a0ccbbc2007b7015b21
Pol Hernandez <polhv@proton.me>
2025-08-27 18:36:20 +0200

    wip

--- cpp09/ex02/src/main.cpp
+++ cpp09/ex02/src/main.cpp
@@ -1,4 +1,4 @@
-//#include "../include/RPN.hpp"
+// #include "../include/RPN.hpp"
 #include <iostream>
 #include <sstream>
 #include <vector>
@@ -6,38 +6,45 @@
 using std::string;
 using std::vector;
 
-typedef vector<vector<int >> VecOfIntVec;
+typedef vector<vector<int> > VecOfIntVec;
 
 void printVecOfVec(VecOfIntVec vecOfVec) {
+  std::cout << "[";
+  for (VecOfIntVec::iterator vIt = vecOfVec.begin(); vIt != vecOfVec.end();
+       ++vIt) {
     std::cout << "[";
-    for (VecOfIntVec::iterator vIt = vecOfVec.begin(); vIt != vecOfVec.end(); ++vIt) {
-        std::cout << "[" ;
-        for (vector<int>::iterator it = vIt->begin(); it != vIt->end(); ++it) {
-            std::cout << *it;
-            if (std::next(it) != vIt->end())
-                std::cout << ", ";
-        }
-        std::cout << "]";
-        if (std::next(vIt) != vecOfVec.end())
-            std::cout << ", ";
+    for (vector<int>::iterator it = vIt->begin(); it != vIt->end(); ++it) {
+      std::cout << *it;
+      if (std::next(it) != vIt->end())
+        std::cout << ", ";
     }
-    std::cout << "]" << std::endl;
+    std::cout << "]";
+    if (std::next(vIt) != vecOfVec.end())
+      std::cout << ", ";
+  }
+  std::cout << "]" << std::endl;
+}
+
+bool argsToVec(int argc, char **argv, VecOfIntVec& vecOfVec) {
+    for (int i = 1; i < argc; i++) {
+      vector<int> vec;
+      int n;
+      std::stringstream ss(argv[i]);
+      if (!(ss >> n))
+        return false;
+      vec.push_back(n);
+      vecOfVec.push_back(vec);
+    }
+    return true;
 }
 
 int main(int argc, char **argv) {
-    if (argc<2) {
-        return 1;
-    }
-    VecOfIntVec vecOfVec;
-    for (int i=1; i < argc; i++) {
-        vector<int> vec;
-        int n;
-        std::stringstream ss(argv[i]);
-        if (!(ss >> n))
-            return 1;
-        vec.push_back(n);
-        vecOfVec.push_back(vec);
-    }
-    printVecOfVec(vecOfVec);
-    return 0;
+  if (argc < 2) {
+    return 1;
+  }
+  VecOfIntVec vecOfVec;
+  if (!argsToVec(argc, argv, vecOfVec))
+      return 1;
+  printVecOfVec(vecOfVec);
+  return 0;
 }
